e2f8e31d4eca47ad5b7f8fa15f53fec5834bab67
Tesla-SHT <2036230339@qq.com>
2023-04-29 22:12:19 +0800

    Modify the navigator&logo and Change files' structure

--- index/index.js
+++ index/index.js
@@ -18,3 +18,42 @@ window.addEventListener('scroll', () => {
     }, 500);
   }
 });
+
+/*loading*/
+
+const waitForImages = () => {
+	const images = [...document.querySelectorAll("img")];
+	const totalImages = images.length;
+  console.log(totalImages);
+	let loadedImages = 0;
+	const loaderEl = document.querySelector(".loader span");
+
+
+
+	images.forEach((image) => {
+		imagesLoaded(image, (instance) => {
+			if (instance.isComplete) {
+				loadedImages++;
+				let loadProgress = loadedImages / totalImages;
+
+				gsap.to(loaderEl, {
+					duration: 1,
+					scaleX: loadProgress,
+					backgroundColor: `hsl(${loadProgress * 120}, 100%, 50%`,
+				});
+
+				if (totalImages == loadedImages) {
+					gsap.timeline()
+						.to(".loading__wrapper", {
+						duration: 0.8,
+						opacity: 0,
+						pointerEvents: "none",
+					})
+						.call(() => init());
+				}
+			}
+		});
+	});
+};
+
+waitForImages();
