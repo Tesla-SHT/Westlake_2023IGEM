76e91e99756f9a1f8a571ab5fe25e65947ca17bd
Tesla-SHT <2036230339@qq.com>
2023-09-07 21:37:59 +0800

    Update header&photo

--- static/header_gsap.js
+++ static/header_gsap.js
@@ -1,3 +1,6 @@
-
-//sequenced one-after-the-other
-gsap.to(".header", {duration: 1.5, y:-100}) ;//notice that there's no semicolon!
+gsap.from(".gaspcol", {
+    scrollTrigger: ".col", delay: 0.3,
+    duration: 1, y: -50, opacity: 0, ease: "power4.out", stagger: 0.3
+  });
+  //sequenced one-after-the-other
+  gsap.to(".header", {duration: 1.5, y:-100}) ;//notice that there's no semicolon!
