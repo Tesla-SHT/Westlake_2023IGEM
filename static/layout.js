f0cda410a4145fd99f9dff2d6f868c0a30fbcfe3
Tesla-SHT <2036230339@qq.com>
2023-09-08 19:04:12 +0800

    Update team&navigator

--- static/layout.js
+++ static/layout.js
@@ -4,29 +4,29 @@ const dropdownMenus = document.querySelectorAll('.dropdown-item');
 // 监听鼠标滚轮事件
 window.addEventListener('scroll', () => {
   // 如果页面滚动距离大于100像素，并且导航栏未缩小，则缩小导航栏
-  if (window.scrollY > 100 && !navbar.classList.contains('navbar-shrink')) {
-    navbar.classList.add('navbar-shrink');    
-    dropdownMenus.forEach(menu => {
-      menu.style.backgroundColor = 'rgba(113, 113, 113, 0.5)';
-    });
-  // 如果页面滚动距离小于100像素，并且导航栏已缩小，则恢复导航栏
-  } else if (window.scrollY < 100 && navbar.classList.contains('navbar-shrink')) {
-    // 为导航栏添加一个渐变过渡效果
-    navbar.style.transition = 'all 0.5s ease-in-out';
-    // 移除 navbar-shrink 类
-    navbar.classList.remove('navbar-shrink');
-    dropdownMenus.forEach(menu => {
-      menu.style.backgroundColor = 'transparent';
-      menu.style.transition = 'all 0.5s ease-in-out';
-    });
-    // 在过渡效果完成后重置导航栏的 transition 属性
-    setTimeout(() => {
-      navbar.style.transition = '';
-      dropdown.style.transition = '';
-    }, 500);
-  }
 });
 
+if (window.scrollY >=0 && !navbar.classList.contains('navbar-shrink')) {
+  navbar.classList.add('navbar-shrink');    
+  dropdownMenus.forEach(menu => {
+    menu.style.backgroundColor = 'rgba(113, 113, 113, 0.5)';
+  });
+// 如果页面滚动距离小于100像素，并且导航栏已缩小，则恢复导航栏
+} else if (window.scrollY <=0 && navbar.classList.contains('navbar-shrink')) {
+  // 为导航栏添加一个渐变过渡效果
+  navbar.style.transition = 'all 0.5s ease-in-out';
+  // 移除 navbar-shrink 类
+  navbar.classList.remove('navbar-shrink');
+  dropdownMenus.forEach(menu => {
+    menu.style.backgroundColor = 'transparent';
+    menu.style.transition = 'all 0.5s ease-in-out';
+  });
+  // 在过渡效果完成后重置导航栏的 transition 属性
+  setTimeout(() => {
+    navbar.style.transition = '';
+    dropdown.style.transition = '';
+  }, 500);
+}
 //回到顶部
 window.onscroll = function () { scrollFunction() };
 function scrollFunction() {// 当网页向下滑动 20px 出现"返回顶部" 按钮
